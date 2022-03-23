4d098f0d3a23b9147f7b82c16a57ba78c965721d
jaieosom <jaieosom@gmail.com>
2022-03-23 13:22:01 -0300

    uni 08.1

--- pages/library3D.js
+++ pages/library3D.js
@@ -30,25 +30,25 @@ export default function Videolibrary3D() {
 
 
 <iframe width="100%" height="515" src="https://conecte3d.com.br/tour360/air50/" title="YouTube video player" 
-frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+ allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
 
 
 <h2>Street Views</h2>
 
 
 <iframe width="100%" height="515" src="https://www.google.com/maps/embed?pb=!4v1648016879655!6m8!1m7!1sCAoSLEFGMVFpcE1TdFNzdVlzdWl4RHRoY1lkR0cteGlzQzlqcXluNS1FOFVtenlz!2m2!1d-9.76764302947521!2d-35.8482216999022!3f120!4f10!5f0.7820865974627469" title="YouTube video player" 
-frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+ allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
 
 
 <h2>Street Views</h2>
 
 <iframe width="100%" height="515" src="https://www.google.com/maps/embed?pb=!4v1648046590209!6m8!1m7!1sCAoSLEFGMVFpcE1zVmRPcm1KaHJJMWh3aWFpeWIxOHFnOHY4X094R2dwYkVfd1gx!2m2!1d-9.7682433!2d-35.837917!3f280!4f10!5f0.7820865974627469" title="YouTube video player" 
-frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+ allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
 
 <h2>Street Views</h2>
 
 <iframe width="100%" height="515" src="https://www.google.com/maps/embed?pb=!4v1648048883984!6m8!1m7!1sCAoSLEFGMVFpcE5jZXhsc0l0MDBpczJCTEhqa25UQTFRUW02T0FXSExZZUhnMEdz!2m2!1d-9.64523847281001!2d-35.70664934813976!3f3.7699485497300467!4f9.19797126539217!5f0.7820865974627469" title="YouTube video player" 
-frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+ allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
 
 
 
